c744f484aad8984c65b77a30632a05477a433f3e
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2018-09-25 21:28:10 +0500

    first test code

---
+++ lp.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main():
+    """ Пример использования longpoll
+        https://vk.com/dev/using_longpoll
+        https://vk.com/dev/using_longpoll_2
+    """
+
+    login, password = 'infraru@rambler.ru', '*ABCd2013&'
+    vk_session = vk_api.VkApi(login, password)
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+
+            if event.from_me:
+                print('От меня для: ', end='')
+            elif event.to_me:
+                print('Для меня от: ', end='')
+
+            if event.from_user:
+                print(event.user_id)
+            elif event.from_chat:
+                print(event.user_id, 'в беседе', event.chat_id)
+            elif event.from_group:
+                print('группы', event.group_id)
+
+            print('Текст: ', event.text)
+            print()
+
+        elif event.type == VkEventType.USER_TYPING:
+            print('Печатает ', end='')
+
+            if event.from_user:
+                print(event.user_id)
+            elif event.from_group:
+                print('администратор группы', event.group_id)
+
+        elif event.type == VkEventType.USER_TYPING_IN_CHAT:
+            print('Печатает ', event.user_id, 'в беседе', event.chat_id)
+
+        elif event.type == VkEventType.USER_ONLINE:
+            print('Пользователь', event.user_id, 'онлайн', event.platform)
+
+        elif event.type == VkEventType.USER_OFFLINE:
+            print('Пользователь', event.user_id, 'офлайн', event)
+            # print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
+
+        else:
+            print(event.type, event.raw[1:])
+
+
+if __name__ == '__main__':
+    main()
